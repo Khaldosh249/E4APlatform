bae965db9d01543618213729ea96935fc49a3887
Khaldosh249 <khalid.wad.rashad@gmail.com>
2025-12-25 14:03:59 +0300

    edit: vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,22 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` (development, production, etc.)
-  // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  const apiUrl = env.VITE_API_URL || 'http://localhost:8000'
-
-  return {
-    plugins: [react()],
-    server: {
-      host: true,
-      port: 5173,
-      allowedHosts: ['e4a.khaldosh.dev', 'api.e4a.khaldosh.dev'],
-      proxy: {
-        '/api': {
-          target: apiUrl,
-          changeOrigin: true,
-        },
-        '/media': {
-          target: apiUrl,
-          changeOrigin: true,
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['e4a.khaldosh.dev', 'api.e4a.khaldosh.dev'],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/media': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
       },
     },
-  }
+  },
 })
